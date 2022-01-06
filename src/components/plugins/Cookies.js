cd0bc8cdeeed71a55a8b18453993a3019381d7ad
wakkana <wakkana@outlook.com>
2022-01-07 00:05:59 +0800

    fix history

--- src/components/plugins/Cookies.js
+++ src/components/plugins/Cookies.js
@@ -12,6 +12,7 @@ export function CookiesGet() {
 export function CookiesSet(_, value) {
   let Name = 'history'
   let a = JSON.parse(localStorage.getItem(Name));
+  if (!a) a = [];
   a.push(value);
   a = [...new Set(a)]
   localStorage.setItem(Name, JSON.stringify(a))
